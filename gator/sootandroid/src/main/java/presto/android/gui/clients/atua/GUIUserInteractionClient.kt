2f6d87cc6b8a0ed6585c335ad11f885ddb10ce07
Chanh Duc Ngo <chanhduc@gmail.com>
2021-07-26 08:11:46 +0200

    Clean project and Update license

--- gator/sootandroid/src/main/java/presto/android/gui/clients/atua/GUIUserInteractionClient.kt
+++ gator/sootandroid/src/main/java/presto/android/gui/clients/atua/GUIUserInteractionClient.kt
@@ -1,11 +1,14 @@
 /*
-* GUIUserInteractionClient.kt - part of the GATOR project
-*
-* Copyright (c) 2019 The Ohio State University
-*
-* This file is distributed under the terms described in LICENSE in the
-* root directory.
-*/
+ * ATUA is a test automation tool for mobile Apps, which focuses on testing methods updated in each software release.
+ * Copyright (C) 2019 - 2021 University of Luxembourg
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 package presto.android.gui.clients.atua
 
